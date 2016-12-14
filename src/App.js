c20a8483eaf69142d61e3303b851648aeb53eb72
Pierre Bertet <bonjour@pierrebertet.net>
2016-12-14 18:38:50 +0000

    Style fix

--- src/App.js
+++ src/App.js
@@ -22,17 +22,15 @@ class App extends Component {
     return (
       <div className='App'>
         <Header />
-        <main>
-          <div className='App-content'>
-            <CardsList
-              title='Top Rated'
-              cards={topCards}
-            />
-            <CardsList
-              title='Featured'
-              cards={featuredCards}
-            />
-          </div>
+        <main className='App-content'>
+          <CardsList
+            title='Top Rated'
+            cards={topCards}
+          />
+          <CardsList
+            title='Featured'
+            cards={featuredCards}
+          />
         </main>
         <Footer />
       </div>
