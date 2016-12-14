dc93bc0fe301d03b781bb70f68156e78b5f7a90e
Pierre Bertet <bonjour@pierrebertet.net>
2016-12-14 16:01:47 +0000

    Base structure + header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import Header from './Header'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <Header />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header(props) {
+  return (
+    <div className='Header'>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
