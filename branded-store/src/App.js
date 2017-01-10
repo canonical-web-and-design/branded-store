c7a7ae513480a2be02fa68ca6921f478e5df96a3
Pierre Bertet <bonjour@pierrebertet.net>
2017-01-10 23:50:10 +0000

    Remove search + real header buttons

--- branded-store/src/App.js
+++ branded-store/src/App.js
@@ -4,12 +4,8 @@ import './App.css'
 import Header from './Header'
 import Footer from './Footer'
 import CardsList from './CardsList'
-import SearchField from './SearchField'
 
 import cards from './cards-data'
-import search from './search'
-
-const SLOW_SEARCH_TIMEOUT = 500
 
 class App extends Component {
 
@@ -17,114 +13,36 @@ class App extends Component {
     super(props)
 
     this.state = {
-      searchTerms: '',
-      searchResults: { groups: [], tags: [] },
-      cancelSearch: null,
-      slowSearchRequest: false,
-      slowSearchRequestTimer: -1,
       topCards: cards(4),
       featuredCards: cards(8),
     }
 
-    this.onSearchChange = this.onSearchChange.bind(this)
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchTerms } = this.state
-    if (prevState.searchTerms !== searchTerms) {
-      this.initSearch(searchTerms)
-    }
-  }
-
-  initSearch(searchTerms) {
-    let cancel = null
-
-    clearTimeout(this.state.slowSearchRequestTimer)
-
-    if (!searchTerms) {
-      this.updateState({
-        slowSearchRequest: false,
-        slowSearchRequestTimer: -1,
-        searchResults: { groups: [], tags: [] },
-        cancelSearch: null,
-      })
-      return
-    }
-
-    // start the search request
-    search(searchTerms, c => cancel = c)
-
-      .then(results => {
-        clearTimeout(this.state.slowSearchRequestTimer)
-        this.updateState({
-          slowSearchRequest: false,
-          slowSearchRequestTimer: -1,
-          searchResults: results,
-          cancelSearch: null,
-        })
-      })
-
-      .catch(() => {
-        clearTimeout(this.state.slowSearchRequestTimer)
-        this.updateState({
-          slowSearchRequest: false,
-          slowSearchRequestTimer: -1,
-          cancelSearch: null,
-        })
-      })
-
-    // start the slow search timer
-    const slowSearchRequestTimer = setTimeout(() => {
-      this.updateState({
-        slowSearchRequest: true,
-        slowSearchRequestTimer: -1,
-      })
-    }, SLOW_SEARCH_TIMEOUT)
-
-    this.updateState({
-      cancelSearch: () => {
-        clearTimeout(this.state.slowSearchRequestTimer)
-        cancel()
-      },
-      slowSearchRequestTimer,
-    })
+    this.onMenuItemClick = this.onMenuItemClick.bind(this)
   }
 
   updateState(update) {
     this.setState(Object.assign({}, this.state, update))
   }
 
-  onSearchChange(value) {
-    const { cancelSearch } = this.state
-    value = value.trim()
-
-    if (cancelSearch) {
-      cancelSearch()
-    }
-
-    this.updateState({
-      searchTerms: value,
-      cancelSearch: null,
-    })
+  onMenuItemClick(id) {
+    console.log(id)
   }
 
   render() {
+
     const {
       topCards,
       featuredCards,
-      searchTerms,
-      searchResults,
-      slowSearchRequest,
     } = this.state
+
     return (
       <div className='App'>
-        <Header />
-
-        <SearchField
-          value={searchTerms}
-          results={searchResults}
-          onChange={this.onSearchChange}
-          wait={slowSearchRequest}
+        <Header
+          menuitems={[
+            { id: 'store', name: 'Store' },
+            { id: 'settings', name: 'Settings' },
+          ]}
+          onMenuItemClick={this.onMenuItemClick}
         />
 
         <main className='App-content'>
--- branded-store/src/Header.js
+++ branded-store/src/Header.js
@@ -1,40 +1,90 @@
-import React from 'react';
-import './Header.css';
-import leftImage from './Header/header-left.png'
-import rightImage from './Header/header-right.png'
-// import logo from './assets/logo.svg'
+import React, { Component } from 'react'
+import './Header.css'
 
-function Header(props) {
-  return (
-    <header className='Header'>
+import defaultLogo from './assets/logo.svg'
 
-      {/*
-      <h1 className='Header-logo'>
-        <img src={logo} alt='Ubuntu' />
-      </h1>
+// item icons
+import iconStore from './Header/store.svg'
+import iconSettings from './Header/settings.svg'
 
-      <nav className='Header-nav'>
-        <ul>
-          <li>My Apps</li>
-          <li>Categories</li>
-        </ul>
-      </nav>
-      */}
+const itemIcons = {
+  store: iconStore,
+  settings: iconSettings,
+}
 
-      <img
-        className='header-left'
-        height="50"
-        src={leftImage}
-        alt='Header left'
-      />
-      <img
-        className='header-right'
-        height="50"
-        src={rightImage}
-        alt='Header right'
-      />
-    </header>
-  )
+const defaultName = 'Ubuntu'
+
+class MenuItem extends Component {
+  constructor(props) {
+    super(props)
+    this.onClick = this.onClick.bind(this)
+  }
+  onClick(event) {
+    event.currentTarget.blur()
+    this.props.onClick(this.props.id)
+  }
+  render() {
+    const { name, id } = this.props
+    return (
+      <li
+        role='button'
+        tabIndex='0'
+        onClick={this.onClick}
+      >
+        {itemIcons[id]? (
+          <img
+            src={itemIcons[id]}
+            alt=''
+          />
+        ) : null}
+        {name}
+      </li>
+    )
+  }
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.onLogoClick = this.onLogoClick.bind(this)
+    this.onMenuItemClick = this.onMenuItemClick.bind(this)
+  }
+  onLogoClick(event) {
+    event.currentTarget.blur()
+  }
+  onMenuItemClick(id) {
+    this.props.onMenuItemClick(id)
+  }
+  render() {
+    const { props } = this
+    return (
+      <header className='Header'>
+        <h1
+          className='Header-logo'
+          role='button'
+          tabIndex='0'
+          onClick={this.onLogoClick}
+        >
+          <img
+            src={props.logo || defaultLogo}
+            alt={props.name || defaultName}
+          />
+        </h1>
+        <nav className='Header-nav'>
+          <ul>
+            {(props.menuitems || []).map(item => (
+              <MenuItem
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                onClick={this.onMenuItemClick}
+              />
+            ))}
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default Header
