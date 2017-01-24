f65265521feb7d0e98076ab92c811deede86f1b9
Vesa Rautiainen <vrautiainen@gmail.com>
2017-01-24 20:51:03 +0200

    End user app interface

---
+++ private-cloud/src/EndUserApp.js
@@ -0,0 +1,205 @@
+import React, { Component } from 'react'
+import './EndUserApp.css'
+
+import If from 'toolkit/If'
+import Header from 'toolkit/Header/Header'
+import Footer from 'toolkit/Footer/Footer'
+
+import createServices from './end-user-services-data'
+
+import HomePage from './HomePage'
+import ServicePage from './ServicePage'
+
+import createHistory from 'history/createBrowserHistory'
+
+const publicUrl = process.env.PUBLIC_URL
+
+// @todo: Replace this url with the real snapweb link on the device
+const snapwebUrl = 'http://localhost:3001/'
+const history = createHistory()
+const sections = ['service']
+
+const brandData = {
+    name: 'Ubuntu',
+    id: 'ubuntu',
+    color: '#E95420',
+}
+
+const bannerData = {
+  photo: 'banner-photo.png', 
+  deviceName: 'Ubuntu self-hosted private cloud suite',
+  deviceId: 'Greetings Lola',
+}
+
+function sectionFromPath(path) {
+  return path === '/' ? 'home' : (
+    sections.find(section => (
+      path.startsWith(`/${section}`)
+    )) || ''
+  )
+}
+
+function serviceIdFromPath(path) {
+  const parts = path.split('/').slice(1)
+  return (parts[0] === 'service' && parts[1]) || ''
+}
+
+function openNewTab(url) {
+  const win = window.open(url, '_blank'); 
+  if (win) {
+    //Browser has allowed it to be opened
+    win.focus();
+  }
+}
+
+function getTimeStamp() {
+  const today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth();
+  const yyyy = today.getFullYear();
+  let hours = today.getHours()
+  let mins = today.getMinutes()
+  let secs = today.getSeconds()
+
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  if(dd<10) dd='0'+dd
+  if(hours<10) hours='0'+hours
+  if(mins<10) mins='0'+mins
+  if(secs<10) secs='0'+secs
+
+  return dd+' '+monthNames[mm]+' '+yyyy+' '+hours+':'+mins+':'+secs;
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      installedServices: createServices(),
+      location: history.location,
+    }
+
+    history.listen(this.handleNavigation.bind(this))
+
+    this.onMenuItemClick = this.onMenuItemClick.bind(this)
+    this.onRequestStop = this.onRequestStop.bind(this)
+    this.onRequestStart = this.onRequestStart.bind(this)
+    this.onRequestAdminPage = this.onRequestAdminPage.bind(this)
+    this.onRequestServicePage = this.onRequestServicePage.bind(this)
+  }
+
+  findServiceById(id) {
+    return this.state.installedServices.find(service => (service.id === id))
+  }
+
+  handleNavigation(location) {
+    this.setState({ location: location })
+    window.scrollTo(0, 0)
+  }
+
+  onMenuItemClick(id) {
+    if (id === 'store') {
+      openNewTab(snapwebUrl)
+    } 
+    if (id === 'home') history.push('/')
+  }
+
+  onOpenService(id) {
+    history.push('/service/' + id)
+  }
+
+  onRequestStop(id) {
+    const newServices = this.state.installedServices
+    const index = this.state.installedServices.findIndex(service => (
+                                                      service.id === id))
+    if (newServices[index].action === 'Running') {
+      newServices[index].action = 'Stopped'
+      newServices[index].history.unshift(['Stopped', getTimeStamp()])
+      this.setState({installedServices: newServices})
+    }
+  }
+  onRequestStart(id) {
+    const newServices = this.state.installedServices
+    const index = this.state.installedServices.findIndex(service => (
+                                                      service.id === id))
+    if (newServices[index].action === 'Stopped') {
+      newServices[index].action = 'Running'
+      newServices[index].history.unshift(['Running', getTimeStamp()])
+      this.setState({installedServices: newServices})
+    }
+  }
+
+  onRequestAdminPage(id) {
+    const service = this.findServiceById(id)
+    if (service) openNewTab(service.adminPage)
+  }
+
+  onRequestServicePage(id) {
+    const service = this.findServiceById(id)
+    if (service) openNewTab(service.servicePage)
+  }
+
+  render() {
+
+    const {
+      location,
+      installedServices,
+    } = this.state
+
+    const currentSection = sectionFromPath(location.pathname)
+
+    const cardImgRootUrl = `${publicUrl}/icons/cards/`
+
+    return (
+      <div className='App'>
+        <Header
+          menuitems={[
+          ]}
+          currentSection={currentSection}
+          onMenuItemClick={this.onMenuItemClick}
+          name={brandData.name}
+          logo={`${publicUrl}/brands/${brandData.id}/logo.png`}
+          customColor={brandData.color}
+        />
+
+        <main className='App-content'>
+          <If cond={currentSection === 'home'}>
+            <HomePage
+              bannerData={bannerData}
+              cardImgRootUrl={cardImgRootUrl}
+              services={installedServices}
+              onOpenService={this.onRequestServicePage}
+            />
+          </If>
+          <If cond={currentSection === 'service'}>
+            <ServicePage
+              cardImgRootUrl={cardImgRootUrl}
+              service={installedServices.find(service => (
+                  service.id === serviceIdFromPath(location.pathname)
+                ))}
+              isRunning={installedServices.find(service => (
+                  service.id === serviceIdFromPath(location.pathname) && service.action === 'Running'
+                ))}
+              onRequestStop={this.onRequestStop}
+              onRequestStart={this.onRequestStart}
+              onRequestAdminPage={this.onRequestAdminPage}
+              onRequestServicePage={this.onRequestServicePage}
+            />
+          </If>
+        </main>
+
+        <Footer 
+          firstLine={null}
+          copyright={`© ${(new Date()).getFullYear()} ${brandData.name}`}
+          logo={`${publicUrl}/brands/${brandData.id}/logo.png`}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ private-cloud/src/end-user-services-data.js
@@ -0,0 +1,86 @@
+
+const services = [
+    {
+      name: 'Wekan',
+      description: 'Wekan is an open source Kanban workflow tool that allows you to create boards, on which cards can be moved around between a number of columns. Boards can have many members, allowing for easy collaboration, just add everyone that should be able to work with you on the board to it, and you are good to go! ',
+      adminPage: 'https://wekan.io/',
+      servicePage: 'https://wekan.io/',
+    },
+    {
+      name: 'Nextcloud',
+      description: 'Nextcloud is a safe home for all your data. Access, share and protect your files, calendars, contacts and communication and more. Nextcloud comes with a feature rich set of pre-integrated services and is developed using a fully open source platform designed to give enterprises full control and privacy.',
+      adminPage: 'https://nextcloud.com/',
+      servicePage: 'https://nextcloud.com/',
+    },
+    {
+      name: 'Rocketchat',
+      description: 'Rocket.Chat is a dynamic and innovative toolkit providing group messaging and video communication and collaboration. It is a great solution for communities and companies wanting to privately host their own chat service.',
+      adminPage: 'https://rocket.chat/',
+      servicePage: 'https://rocket.chat/',
+    },
+    {
+      name: 'Gogs',
+      description: 'Gogs is a easy to use code repository service based on Git designed for self hosted environments. Gogs provides a full code management and distribution and is designed to help developers accelerate projects within a fully private environment.',
+      adminPage: 'https://gogs.io/',
+      servicePage: 'https://gogs.io/',
+    },
+    {
+      name: 'Spreed',
+      description: 'Spreed is a private video and chat messaging service based on WebRTC and is available through Nextcloud. The service brings you easy to use web conferencing, one to one calls, chat and is designed to give the enterprise full control of its communication.',
+      adminPage: 'https://spreed.me/',
+      servicePage: 'https://spreed.me/',
+      image: 'spreed2'
+    },
+    {
+      name: 'iRedMail',
+      description: 'Iredmail is the complete self-hosted, open source mail server solution for enterprises. With iRedMail, you can deploy a full feature mail server in several minutes.',
+      adminPage: 'http://www.iredmail.org/',
+      servicePage: 'http://www.iredmail.org/',
+    },
+    {
+      name: 'Collabora',
+      description: 'Collaborative editing with LibreOffice using your own private cloud. Collabora Online is for enterprises that want a powerful self-hosted office suite that protects their privacy and allows them to keep full control of their sensitive corporate data.',
+      adminPage: 'https://www.collabora.com/',
+      servicePage: 'https://www.collabora.com/',
+    }
+  ]
+
+const defaultHistory = [
+  ['Running', '19 Jan 2017 00:00:00']
+]
+
+const defaultDetails = [
+  ['Category', 'Databases'],
+  ['Size', '65.7MB'],
+  ['Version', '3.7'],
+  ['Channel', 'Stable'],
+  ['Updated', '12 Aug 2016 12:37:06'],
+]
+
+const defaultInterfaces = [
+  'Network',
+  'Network Bind',
+  'Mount Observe',
+]
+
+// Generate services data
+function servicesData() {
+
+  // add attributes that are the same for each service
+  services.forEach(function (element) {
+    
+    element.id = element.name.toLowerCase()
+
+    if (!element.image) element.image = element.name.toLowerCase()
+    if (!element.details) element.details = defaultDetails.slice()
+    if (!element.interfaces) element.interfaces = defaultInterfaces.slice()
+    if(!element.history) element.history = defaultHistory.slice()
+
+    // @todo: change this attribute to 'status' once the toolkit CardList api has been improved
+    element.action = 'Sign-in'
+  })
+    
+  return services
+}
+
+export default servicesData
