4571f240dd34a1d72ac8ce4d32c05032457a4e0f
Pierre Bertet <bonjour@pierrebertet.net>
2017-01-30 11:27:43 +0000

    Remove private-cloud and the toolkit

--- private-cloud/src/index-enduser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './EndUserApp.js'
-import './index.css'
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
