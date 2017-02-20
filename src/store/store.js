6dfe68df6674094ac7713ff249e6a343508c20de
Vesa Rautiainen <vrautiainen@gmail.com>
2017-02-20 21:21:07 +0200

    Check for interfaces so that won't crash if they don't exist.

--- src/store/store.js
+++ src/store/store.js
@@ -49,7 +49,7 @@ function createAllSnaps(customSnapsData) {
     .map(snap => {
       const id = snap.id || (snap.snapId || '').trim() || snap.name.toLowerCase().split(' ').join('-')
       const type = snap.type || 'Snap'
-      const interfaces = snap.interfaces.split(',')
+      const interfaces = (snap.interfaces || '').split(',')
       const preinstalled = snap.preinstalled || false
       const status = (
         installedIds.includes(id) || snap.preinstalled
