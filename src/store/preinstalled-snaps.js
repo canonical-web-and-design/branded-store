550156945bd649f2d900d0320e923ba507fd5c8a
David Barth <david.barth@canonical.com>
2017-02-21 12:27:50 +0100

    update pre-installed snaps details

--- src/store/preinstalled-snaps.js
+++ src/store/preinstalled-snaps.js
@@ -7,8 +7,10 @@ const snaps = [
     price: 'free',
     rating: -1,
     category: 'kernel app',
-    interfaces: 'network,network-bind,mount-observe',
-    description: '',
+    interfaces: 'n/a',
+    description: 'The generic Ubuntu kernel package as a snap',
+    size: '128.01MB',
+    version: '4.4.0-53-1',
     preinstalled: true,
   },
   {
@@ -19,8 +21,10 @@ const snaps = [
     price: 'free',
     rating: -1,
     category: 'device manager',
-    interfaces: 'network,network-bind,mount-observe',
-    description: '',
+    interfaces: 'network,network-bind,snapd-control,timeserver-control',
+    description: "This service allows you to manage your Ubuntu Core device from a web interface or REST API.\n\nFeatures include:\n - manage updates to the system.\n - control the state of other snappy packages.\n - browse the store to install new snappy packages.",
+    size: '4.71MB',
+    version: '0.21.2',
     preinstalled: true,
   },
   {
@@ -31,10 +35,14 @@ const snaps = [
     price: 'free',
     rating: -1,
     category: 'OS app',
-    interfaces: 'network,network-bind,mount-observe',
-    description: '',
+    interfaces: 'core:account-control,core:alsa,core:avahi-observe,core:bluetooth-control,core:browser-support,core:camera,...',
+    description: 'The core runtime environment for snapd',
+    size: '79.74MB',
+    version: '16.04.1',
     preinstalled: true,
   },
+  // below, the full interface list for reference
+  // interfaces: 'core:account-control,core:alsa,core:avahi-observe,core:bluetooth-control,core:browser-support,core:camera,core:core-support,core:cups-control,core:dcdbas-control,core:docker-support,core:firewall-control,core:fuse-support,core:gsettings,core:hardware-observe,core:home,core:io-ports-control,core:kernel-module-control,core:libvirt,core:locale-control,core:log-observe,core:lxd-support,core:modem-manager,core:mount-observe,core:network,core:network-bind,core:network-control,core:network-manager,core:network-observe,core:network-setup-observe,core:ofono,core:opengl,core:openvswitch,core:openvswitch-support,core:optical-drive,core:physical-memory-control,core:physical-memory-observe,core:ppp,core:process-control,core:pulseaudio,core:raw-usb,core:removable-media,core:screen-inhibit-control,core:shutdown,core:snapd-control,core:system-observe,core:system-trace,core:time-control,core:timeserver-control,core:timezone-control,core:tpm,core:uhid,core:unity7,core:upower-observe,core:x11',
 ]
 
 export default snaps
